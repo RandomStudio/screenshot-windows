cecc36f82d96bf67683f0749766cd0113879e49e
Thijs van Hoof <thijs.v.h@gmail.com>
2018-08-27 11:41:46 +0200

    GetEncoderClsid

--- addon.cc
+++ addon.cc
@@ -1,9 +1,14 @@
 #define ERR_NO_JPEG_ENCODER -1
 
+#include "minmax.h"
 #include <node.h>
 #include <windows.h>
+#include <gdiplus.h>
+#pragma comment(lib, "gdiplus")
 
 namespace addon {
+
+using namespace Gdiplus;
 
 using v8::FunctionCallbackInfo;
 using v8::Isolate;
@@ -13,6 +18,34 @@ using v8::Integer;
 using v8::Value;
 
 // PRIVATE FUNCTIONS
+
+int GetEncoderClsid(const WCHAR* format, CLSID* pClsid) {
+  UINT num = 0;  // number of image encoders
+  UINT size = 0; // size of the image encoder array in bytes
+
+  ImageCodecInfo* pImageCodecInfo = NULL;
+
+  GetImageEncodersSize(&num, &size);
+  if(size == 0)
+    return -1; // Failure
+
+  pImageCodecInfo = (ImageCodecInfo*)(malloc(size));
+  if(pImageCodecInfo == NULL)
+    return -1; // Failure
+
+  GetImageEncoders(num, size, pImageCodecInfo);
+
+  for (UINT j = 0; j < num; ++j) {
+    if (wcscmp(pImageCodecInfo[j].MimeType, format) == 0) {
+      *pClsid = pImageCodecInfo[j].Clsid;
+      free(pImageCodecInfo);
+      return j; // Success
+    }
+  }
+
+  free(pImageCodecInfo);
+  return -1; // Failure
+}
 
 // EXPOSED FUNCTIONS
 
