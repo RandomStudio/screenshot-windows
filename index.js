999f750b5ab0b2c7984cd478a045d1f7bd3b7610
Thijs van Hoof <thijs.v.h@gmail.com>
2018-08-28 15:32:49 +0200

    takeScreenshot test

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ if (process.argv.includes('getHeight')) {
 }
 
 if (process.argv.includes('takeScreenshot')) {
-	console.log('result:', addon.takeScreenshot());
+	console.log('result:', addon.takeScreenshot('screenshot.jpg', 80, 1280, 720));
 }
 
 // EXPOSED FUNCTION
